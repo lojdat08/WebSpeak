8159113ebe3a4bba71148d5d93ac8ac439df1991
ᡕᠵデᡁ᠊╾━ ✷ HLADIKO <hladiko.08@spst.eu>
2025-12-02 21:16:28 +0100

    grafika
    grafická úprava a oddělání footeru

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>WebSpeak</title>
+    <link rel="stylesheet" href="index.css" type="text/css" />
+    <link rel="icon" type="image/png" href="../logo.png">
+</head>
+
+<body>
+    <div class="page-wrapper">
+        <header>
+            <div class="header-content">
+                <img src="../logo.png" width=60vw height=60vh>
+                <h1>Webspeak</h1>
+                <div class="buttons">
+                    <a href="login.php">
+                        <button class="headerButton" name="loginForm">Přihlášení</button>
+                    </a>
+                    <a href="register.php">
+                        <button class="headerButton" name="registerForm">Registrace</button>
+                    </a>
+                </div>
+            </div>
+        </header>
+        <div class="main">
+            <h2>Úvod</h2>
+            <p>Webspeak je komunikační platforma pro všechny co chtějí ty stare nepřeplácané komunikační aplikace.</p>
+        </div>
+    </div>
+</body>
+
+</html>
